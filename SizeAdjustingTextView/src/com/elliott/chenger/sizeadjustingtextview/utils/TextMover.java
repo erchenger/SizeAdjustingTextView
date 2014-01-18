da7cc63cf7b8fce20e7646e23d62b12d95ef95cd
Elliott Chenger <erchenger@gmail.com>
2014-01-18 11:57:46 -0600

    Initial commit.

---
+++ SizeAdjustingTextView/src/com/elliott/chenger/sizeadjustingtextview/utils/TextMover.java
@@ -0,0 +1,16 @@
+package com.elliott.chenger.sizeadjustingtextview.utils;
+
+import com.elliott.chenger.sizeadjustingtextview.view.SizeAdjustingTextView;
+
+public class TextMover {
+
+	public static void moveTextFromCellToCell(SizeAdjustingTextView startingCell,
+			SizeAdjustingTextView endCell){
+		
+	}
+	
+	public static void moveNewTextIntoCell(SizeAdjustingTextView targetCell,
+			String text){
+		targetCell.setText(text);
+	}
+}
